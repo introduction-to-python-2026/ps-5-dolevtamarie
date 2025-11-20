766f4b410509342994c99f0a7f68494e5114914a
dolevtamarie <dolevtamarie@gmail.com>
2025-11-20 15:45:22 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -4,12 +4,15 @@
 def split_before_each_uppercases(formula):
     if not formula:
         return []
+    
     parts = []
-    start = 0
+    start = 0 
+    
     for i in range(1, len(formula)):
         if formula[i].isupper():
             parts.append(formula[start:i])
             start = i
+            
     parts.append(formula[start:])
     return parts
 
